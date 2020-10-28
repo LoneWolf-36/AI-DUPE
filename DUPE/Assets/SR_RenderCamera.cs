633d82bee26904ce56a0caf3f935c58e2248be4a
LoneWolf-36 <yatakula36@gmail.com>
2020-10-28 17:48:29 +0530

    UPDATED 28/10 DUPE REMASTERED

---
+++ DUPE/Assets/SR_RenderCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SR_RenderCamera : MonoBehaviour
+{
+    Camera cam;
+    public int resWidth = 256;
+    public int resHeight = 256;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        InvokeRepeating("Snap", 1.0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Snap()
+    {
+        Texture2D camtex = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+        cam.Render();
+        RenderTexture.active = cam.targetTexture;
+        camtex.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+        camtex.Apply();
+        byte[] bytes = camtex.EncodeToPNG();
+        string FileName = IMG_NAME();
+        System.IO.File.WriteAllBytes(FileName, bytes);
+        Debug.Log("IMG taken");
+
+    }
+
+    string IMG_NAME()
+    {
+        return string.Format("{0}/IMG/ IMG_{1}.png", Application.dataPath, System.DateTime.Now.ToString("yyyy-MM-dd"));
+    }
+}
--- DUPE/Assets/Scripts/CAMREC.cs
+++ DUPE/Assets/Scripts/CAMREC.cs
@@ -1,39 +1,35 @@
-﻿ using UnityEngine;
- using System.Collections;
-[RequireComponent(typeof(Camera))]
-public class CAMREC : MonoBehaviour {
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class CAMREC : MonoBehaviour
+{
     Camera cam;
-    public int resWidth = 2550;
-    public int resHeight = 3300;
-
-    void Awake()
+    public int resWidth = 256;
+    public int resHeight = 256;
+    // Start is called before the first frame update
+    void Start()
     {
-        cam = GetComponent<Camera>();
-        Debug.Log("IMG needs to be taken");
-        Debug.Log(cam);
+        cam = GetComponent<Camera>();    
     }
 
-
-    void LateUpdate()
+    // Update is called once per frame
+    void Update()
     {
         Texture2D camtex = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
         cam.Render();
-        Debug.Log("Update");
         RenderTexture.active = cam.targetTexture;
-        camtex.ReadPixels(new Rect(0, 0,resWidth, resHeight), 0, 0);
-        cam.targetTexture = null;
-        RenderTexture.active = null; // JC: added to avoid errors
-        Destroy(cam.targetTexture);
-        byte[] bytes = camtex.EncodeToJPG();
-        string fileName = jpgName();
-        System.IO.File.WriteAllBytes(fileName, bytes);
+        camtex.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+        camtex.Apply();
+        byte[] bytes = camtex.EncodeToPNG();
+        string FileName = IMG_NAME();
+        System.IO.File.WriteAllBytes(FileName, bytes);
         Debug.Log("IMG taken");
+
     }
 
-    string jpgName()
+    string IMG_NAME()
     {
-        return string.Format("{0}/IMG/img_{1}", Application.dataPath, System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+        return string.Format("{0}/IMG/ IMG_{1}.png", Application.dataPath, System.DateTime.Now.ToString("yyyy-MM-dd"));
     }
 }
-    
