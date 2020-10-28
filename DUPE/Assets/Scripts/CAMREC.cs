a9b8904e6d99678f226a265ff4495c748401ad30
LoneWolf-36 <yatakula36@gmail.com>
2020-10-28 11:43:55 +0530

    tried adding capture IMGs

---
+++ DUPE/Assets/Scripts/CAMREC.cs
@@ -0,0 +1,39 @@
+﻿ using UnityEngine;
+ using System.Collections;
+[RequireComponent(typeof(Camera))]
+public class CAMREC : MonoBehaviour {
+
+    Camera cam;
+    public int resWidth = 2550;
+    public int resHeight = 3300;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+        Debug.Log("IMG needs to be taken");
+        Debug.Log(cam);
+    }
+
+
+    void LateUpdate()
+    {
+        Texture2D camtex = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+        cam.Render();
+        Debug.Log("Update");
+        RenderTexture.active = cam.targetTexture;
+        camtex.ReadPixels(new Rect(0, 0,resWidth, resHeight), 0, 0);
+        cam.targetTexture = null;
+        RenderTexture.active = null; // JC: added to avoid errors
+        Destroy(cam.targetTexture);
+        byte[] bytes = camtex.EncodeToJPG();
+        string fileName = jpgName();
+        System.IO.File.WriteAllBytes(fileName, bytes);
+        Debug.Log("IMG taken");
+    }
+
+    string jpgName()
+    {
+        return string.Format("{0}/IMG/img_{1}", Application.dataPath, System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    }
+}
+    
---
+++ DUPE/Assets/Scripts/FAKE.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FAKE : MonoBehaviour
+{
+    Camera cam;
+    public int resWidth = 2550;
+    public int resHeight = 3300;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.gameObject.SetActive(true);
+        Debug.Log("IMG needs to be faken");
+    }
+}
